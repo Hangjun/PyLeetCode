fa64a5de119056da50a3c7559931a2539319f93b
Hangjun <ajxuhangjun2007@gmail.com>
2019-08-12 23:45:54 -0700

    Update 015. 3Sum.py

--- 015. 3Sum.py
+++ 015. 3Sum.py
@@ -22,34 +22,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        if (len(nums) < 3): return []
-        
+        res = []
+        if (len(nums) < 3):
+            return res
         nums.sort()
-        res = set()
-        for i, v in enumerate(nums[:-2]): # except the last two elements
-            # de-duplicate
-            if i > 0 and v == nums[i-1]: continue
-            left, right = i+1, len(nums)-1
-            while left < right:
-                tmpSum = v + nums[left] + nums[right]
-                if tmpSum == 0:
-                    res.add((v, nums[left], nums[right]))
-                    left += 1
-                    right -= 1
-                    # de-duplicate
-                    while left < right and nums[left] == nums[left-1]: left += 1
-                    while left < right and nums[right] == nums[right+1]: right -= 1
-                elif tmpSum < 0:
-                    left += 1
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif nums[i] + nums[l] + nums[r] < 0:
+                    l += 1
                 else:
-                    right -= 1
-        
-        return map(list, res)
-                    
-"""
-Remark：
-1. 我们当然可以把这个问题拆解为一个2Sum外面套一个for loop，但是这并不是最优解，因为2Sum还需要一个dictionary占用多余的空间复杂度。
-2. 上面由于我们用了Python的set，其实会自动帮我们去重（假如我没有理解错Python set的定义的话）。但是我们要尽可能的少依赖自带的数据结构的优势，而是从
-算法上更加严谨的implement。
-"""
-    
+                    r -= 1
+        return res
