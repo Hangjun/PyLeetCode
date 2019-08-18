c6285ccb56c9128ce427c018f2b6123f423c047b
Hangjun <ajxuhangjun2007@gmail.com>
2019-08-18 15:11:54 -0700

    Update 367. Valid Perfect Square.py

--- 367. Valid Perfect Square.py
+++ 367. Valid Perfect Square.py
@@ -13,3 +13,23 @@ Input: 14
 Output: false
 """
 
+# Binary Search. Very similar to sqrt(num). Time: O(logn), Space: O(1).
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 1:
+            return True
+        l, r = 1, int(math.ceil(num/2))
+        while l <= r:
+            mid = l + (r - l) / 2
+            midSquare = mid * mid
+            if midSquare == num:
+                return True
+            elif midSquare < num:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
