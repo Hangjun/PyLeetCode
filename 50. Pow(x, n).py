caf1541f19a4874823cd88a54d377d164aed427f
Hangjun <ajxuhangjun2007@gmail.com>
2019-08-18 14:20:51 -0700

    Create 50. Pow(x, n).py

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,42 @@
+"""
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+"""
+
+# Divide-and-conquer. Time: O(logn), Space: O(1)
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1.0
+        if n == 1:
+            return x
+        if n < 0:
+            return 1.0 / self.myPow(x, -n)
+        
+        tmp = self.myPow(x, n/2)
+        if n % 2 == 0:
+            return tmp * tmp
+        else:
+            return tmp * tmp * x
