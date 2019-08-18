1e25584ed30382bf7e02b634b8122a2b1f1f3d0a
Hangjun <ajxuhangjun2007@gmail.com>
2019-08-18 14:29:17 -0700

    Update 69. Sqrt(x).py

--- 69. Sqrt(x).py
+++ 69. Sqrt(x).py
@@ -43,3 +43,25 @@ class Solution(object):
         
         # l == r + 1
         return r
+
+# A different (more consise implementation)
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        l, r = 1, int(math.ceil(x/2))
+        while True:
+            mid = l + (r - l) / 2
+            if (mid > x / mid):
+                r = mid -1
+            else:
+                if ((mid + 1) > x / (mid + 1)):
+                    return mid
+                else:
+                    l = mid+1
