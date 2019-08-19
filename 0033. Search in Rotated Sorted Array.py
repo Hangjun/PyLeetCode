ef3b5dbfed1c5d78c6e47137146bc91ae968b3fa
Hangjun <ajxuhangjun2007@gmail.com>
2019-08-18 18:05:31 -0700

    Update 0033. Search in Rotated Sorted Array.py

--- 0033. Search in Rotated Sorted Array.py
+++ 0033. Search in Rotated Sorted Array.py
@@ -19,3 +19,31 @@ Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 """
 
+# Binary Search. Time: O(logn), Space: O(1)
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right - left) / 2
+            if nums[mid] == target:
+                return mid
+            # nums[mid: right] is sorted
+            if nums[mid] < nums[right]:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            # nums[left, mid] is sorted
+            else:
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
