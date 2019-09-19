92d177f6ae96a4f638aea7740f2716cb5ae801ca
Hangjun <ajxuhangjun2007@gmail.com>
2019-09-18 22:39:38 -0700

    Create p349. Intersection of Two Arrays.py

---
+++ p349. Intersection of Two Arrays.py
@@ -0,0 +1,42 @@
+"""
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+"""
+
+# Hash Set. Time: O(m+n), Space: O(m+n).
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
+        
+# We can improve the space complexity to min(m,n) by using one hash set and check against the other list.
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        s = set(nums1)
+        res = []
+        for n in nums2:
+            if n in s:
+                res.append(n)
+                s.remove(n)
+        return res
