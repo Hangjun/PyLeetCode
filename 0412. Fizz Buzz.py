1673b67413785a16697fdc80b903fb58d8469298
Hangjun <ajxuhangjun2007@gmail.com>
2019-09-22 00:55:03 -0700

    Update 0412. Fizz Buzz.py

--- 0412. Fizz Buzz.py
+++ 0412. Fizz Buzz.py
@@ -45,3 +45,12 @@ class Solution(object):
             else:
                 res.append(str(i))
         return res
+
+# Or more concisely
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return ["Fizz" * (not i % 3) + "Buzz" * (not i % 5) or str(i) for i in range(1, n+1)]
