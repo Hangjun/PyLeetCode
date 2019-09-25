132493eb4457dc0534c8fd9263bb92c6250dd920
Hangjun <ajxuhangjun2007@gmail.com>
2019-09-25 10:22:02 -0700

    Update 0200. Number of Islands.py

--- 0200. Number of Islands.py
+++ 0200. Number of Islands.py
@@ -58,4 +58,18 @@ class Solution(object):
 
 # Union-Find Solution.
 
-# Very sick Python solution: Sink the island.
+# If we can modify the input, we can re-write the DFS solution by "sinking" the islands:
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        def sink(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+                grid[i][j] = '0' # sink that node down
+                # recursively sink the neighrboing connected nodes with '1'
+                map(sink, (i-1, i, i+1, i), (j, j+1, j, j-1))
+                return 1
+            return 0
+        return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
