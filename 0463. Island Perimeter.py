1b50ae13a6d9e150c4d256e2c54f4feb235214d9
Hangjun <ajxuhangjun2007@gmail.com>
2019-09-29 21:06:14 -0700

    Create 0463. Island Perimeter.py

---
+++ 0463. Island Perimeter.py
@@ -0,0 +1,42 @@
+"""
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+Example:
+
+Input:
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Output: 16
+
+Explanation: The perimeter is the 16 yellow stripes in the image below:
+"""
+
+# DFS. Time: O(mn), Space: O(1).
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+        count = 0
+        repeat = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    count += 1
+                    if (i > 0 and grid[i-1][j] == 1): 
+                        repeat += 1
+                    if (j > 0 and grid[i][j-1] == 1):
+                        repeat += 1
+        return 4 * count - 2 * repeat
