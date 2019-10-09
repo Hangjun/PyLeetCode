61ec2b4518b5585c7348727bf5e8f86e29480b37
Hangjun <ajxuhangjun2007@gmail.com>
2019-10-08 21:59:47 -0700

    Create 0144. Binary Tree Preorder Traversal.py

---
+++ 0144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,67 @@
+"""
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+# Solution #1: Iterative solution with stack. Time: O(n), Space: O(1).
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+        cur = root
+        while cur or stack:
+            if cur:
+                res.append(cur.val)
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                cur = cur.right
+        return res
+
+# Solution #2: Different implementation same algorithm.
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            if not cur:
+                continue
+            res.append(cur.val)
+            stack.append(cur.right)
+            stack.append(cur.left)
+        return res
