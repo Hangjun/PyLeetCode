dd04f9d842a6f68995132b52577a531f8599fb82
Hangjun <ajxuhangjun2007@gmail.com>
2019-10-12 17:10:06 -0700

    Update 0200. Number of Islands.py

--- 0200. Number of Islands.py
+++ 0200. Number of Islands.py
@@ -22,37 +22,22 @@ Output: 3
 """
 
 # DFS Solution. Time: O(mn), Space: O(mn)
-class Solution(object):
-    def numIslands(self, grid):
+def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid or not grid[0]:
+        def dfs(i, j, visited):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and not visited[i][j] and grid[i][j] == '1':
+                visited[i][j] = True
+                map(dfs, (i-1, i, i+1, i), (j, j+1, j, j-1), (visited, visited, visited, visited))
+                return 1
             return 0
+        
+        if not grid or not grid[0]: return 0
         m, n = len(grid), len(grid[0])
         visited = [[False] * n for i in range(m)]
-        res = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] != '1' or visited[i][j]:
-                    continue
-                self.dfs(grid, i, j, visited)
-                res += 1
-        return res
-    
-    def dfs(self, grid, x, y, visited):
-        visited[x][y] = True
-        dx = [-1, 0, 1, 0]
-        dy = [0, 1, 0, -1]
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if self.inBound(grid, nx, ny) and not visited[nx][ny] and grid[nx][ny] == '1':
-                self.dfs(grid, nx, ny, visited)
-                
-    def inBound(self, grid, x, y):
-        return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
+        return sum(dfs(i, j, visited) for i in range(m) for j in range(n))
 
 # BFS Solution. Time: O(mn), Space: O(mn)
 import Queue as queue
