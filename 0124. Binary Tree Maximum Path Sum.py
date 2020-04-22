da4a2da42a8de07ef5680936bec031926e6d93fc
Hangjun <ajxuhangjun2007@gmail.com>
2020-04-22 11:27:37 -0700

    Update 0124. Binary Tree Maximum Path Sum.py

--- 0124. Binary Tree Maximum Path Sum.py
+++ 0124. Binary Tree Maximum Path Sum.py
@@ -50,15 +50,18 @@ class Solution(object):
         :rtype: int
         """
         if not root:
-            return 0
+            return self.res
+        # global variable associated with the class
         self.res = root.val
         self.dfs(root)
         return self.res
     
+    # Returns the maxium path sum either starting from root or ending at root
     def dfs(self, root):
         if not root:
             return 0
-        left_path = self.dfs(root.left)
-        right_path = self.dfs(root.right)
-        self.res = max(self.res, max(left_path, 0) + max(right_path, 0) + root.val)
-        return max(max(left_path, right_path), 0) + root.val
+        left_path_sum = self.dfs(root.left)
+        right_path_sum = self.dfs(root.right)
+        self.res = max(self.res, left_path_sum + right_path_sum + root.val)
+        # Choose either left path, or right path, or an empty path
+        return max(root.val + max(left_path_sum, right_path_sum), 0)
