075b3c99012d4a6b041c50bfdb1046226d46616e
Hangjun <ajxuhangjun2007@gmail.com>
2020-04-26 23:13:11 -0700

    Create 1002. Find Common Characters.py

---
+++ 1002. Find Common Characters.py
@@ -0,0 +1,48 @@
+"""
+Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).  For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: ["cool","lock","cook"]
+Output: ["c","o"]
+ 
+
+Note:
+
+1 <= A.length <= 100
+1 <= A[i].length <= 100
+A[i][j] is a lowercase letter
+"""
+
+# Python Counter intersection. Time: O(nk), Space: O(nk). n = number of strings, k = average length of strings
+from collections import Counter
+class Solution(object):
+    def commonChars(self, A):
+        """
+        :type A: List[str]
+        :rtype: List[str]
+        """
+        if not A:
+            return None
+        res = Counter(A[0])
+        for s in A:
+            res &= Counter(s)
+        return list(res.elements())
+
+# Here is a one-liner implementation of the same algorithm:
+from collections import Counter
+class Solution(object):
+    def commonChars(self, A):
+        """
+        :type A: List[str]
+        :rtype: List[str]
+        """
+        return list(reduce(Counter.__and__, map(Counter, A)).elements())
