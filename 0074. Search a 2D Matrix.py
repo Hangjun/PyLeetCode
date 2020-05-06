3fa08f0b134bf3582ae4e9abc79c5f25d35d0f74
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-06 00:01:31 -0700

    Create 0074. Search a 2D Matrix.py

---
+++ 0074. Search a 2D Matrix.py
@@ -0,0 +1,49 @@
+"""
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+"""
+
+# Solution #1: Stepwise Binary Search. Time: O(max(m, n)), Space: O(1).
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        if target < matrix[0][0] or target > matrix[m-1][n-1]:
+            return False
+        row, col = m-1, 0
+        while row >= 0 and col < n:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                row -= 1
+            else:
+                col += 1
+        return False
