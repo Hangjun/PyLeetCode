d7cb7f45356c9370286866e0fa1221ad6c39d9dc
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-06 14:14:35 -0700

    Create 0382. Linked List Random Node.py

---
+++ 0382. Linked List Random Node.py
@@ -0,0 +1,49 @@
+"""
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+"""
+
+# Reservior Sampling. Time: O(n), Space: O(1).
+from random import randint
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+
+    def __init__(self, head):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        :type head: ListNode
+        """
+        self.head = head
+
+    def getRandom(self):
+        """
+        Returns a random node's value.
+        :rtype: int
+        """
+        res = None
+        count = 0
+        curNode = self.head
+        while curNode:
+            count += 1
+            if randint(0, count-1) == 0:
+                res = curNode.val
+            curNode = curNode.next
+        return res
