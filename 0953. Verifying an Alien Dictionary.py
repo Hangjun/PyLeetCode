695e109c0af0b89cb6d21bfb5c8fa4a49d0e78fe
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-10 23:22:42 -0700

    Create 0953. Verifying an Alien Dictionary.py

---
+++ 0953. Verifying an Alien Dictionary.py
@@ -0,0 +1,53 @@
+"""
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+ 
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are English lowercase letters.
+"""
+# Time: O(nk), Space: O(1). n = len(words), k = average length of each word.
+class Solution(object):
+    def isAlienSorted(self, words, order):
+        """
+        :type words: List[str]
+        :type order: str
+        :rtype: bool
+        """
+        charToIndex = {}
+        for i, c in enumerate(order):
+            charToIndex[c] = i
+        for i in range(len(words)-1):
+            s, t = words[i], words[i+1]
+            n = min(len(s), len(t))
+            for j in range(n):
+                if s[j] != t[j]:
+                    if charToIndex[s[j]] > charToIndex[t[j]]:
+                        return False
+                    break
+                if j == n-1 and len(s) > len(t):
+                    return False
+        return True
