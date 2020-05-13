c6e2173c086e52bfdc5722b3857bd12b08043088
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-12 17:37:47 -0700

    Update 0283. Move Zeroes.py

--- 0283. Move Zeroes.py
+++ 0283. Move Zeroes.py
@@ -7,24 +7,17 @@ Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 """
-
+# Two Pointers. Time: O(n), Space: O(1). In-place.
 class Solution(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
-        """
-        :invariant: nums[0,...,left-1] contains only nonzeros
-        :nums[left,...,right-1] contains only zeros
-        :when right == nums.size() the requirement is then met
-        """
-        left, right = 0, 0
-        while right < len(nums):
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right += 1
-            else:
-                right += 1
+        left = 0
+        # invariant: [0, left) = no zero, [left, right) = zeros, [right, n-1] = unexplored
+        for right in range(len(nums)):
+            if not nums[right]:
+                continue
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
