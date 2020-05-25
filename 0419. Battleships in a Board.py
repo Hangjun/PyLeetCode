9acb0ebba09d16453f083d4090abd6cae00251c0
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-24 17:57:43 -0700

    Create 0419. Battleships in a Board.py

---
+++ 0419. Battleships in a Board.py
@@ -0,0 +1,29 @@
+"""
+Given an 2D board, count how many battleships are in it. The battleships are represented with 'X's, empty slots are represented with '.'s. You may assume the following rules:
+You receive a valid board, made of only battleships or empty slots.
+Battleships can only be placed horizontally or vertically. In other words, they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
+At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+Example:
+X..X
+...X
+...X
+In the above board there are 2 battleships.
+Invalid Example:
+...X
+XXXX
+...X
+This is an invalid board that you will not receive - as battleships will always have a cell separating between them.
+Follow up:
+Could you do it in one-pass, using only O(1) extra memory and without modifying the value of the board?
+"""
+
+# Time: O(mn), 1-Pass. Space: O(1). Since all battleships are strips, it suffices to just count all the top left 'X's in the board.
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        m, n = len(board), len(board[0])
+        # count all the top left 'X's: nothing on its top or left
+        return sum(board[i][j] == 'X' and (i == 0 or board[i-1][j] != 'X') and (j == 0 or board[i][j-1] != 'X') for i in range(m) for j in range(n))
