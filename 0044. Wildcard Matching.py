5e7bf3d58922b343539d89b3a863562f31094184
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-25 10:48:39 -0700

    Create 0044. Wildcard Matching.py

---
+++ 0044. Wildcard Matching.py
@@ -0,0 +1,76 @@
+"""
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input:
+s = "aa"
+p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+Example 3:
+
+Input:
+s = "cb"
+p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+Example 4:
+
+Input:
+s = "adceb"
+p = "*a*b"
+Output: true
+Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+Example 5:
+
+Input:
+s = "acdcb"
+p = "a*c?b"
+Output: false
+"""
+
+# Iterative Solution. Time: O(min(m, n)) for the best case. Space: O(1).
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if not p:
+            return not s
+        sStar = pStar = -1
+        i = j = 0
+        while i < len(s):
+            if j < len(p) and (s[i] == p[j] or p[j] == '?'):
+                i += 1
+                j += 1
+            elif j < len(p) and p[j] == '*':
+                sStar = i
+                pStar = j
+                j += 1
+            elif pStar != -1:
+                j = pStar + 1
+                sStar += 1
+                i = sStar
+            else:
+                return False
+        while j < len(p) and p[j] == '*':
+            j += 1
+        return j == len(p)
